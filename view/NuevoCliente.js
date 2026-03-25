e24f7cddd7d2870d75f134948265b92cea92e151
Omar <omar.garcia@morsa.com.mx>
2026-03-25 10:09:15 -0600

    Añadiendo el resto de CAMPOS al FORM

--- view/NuevoCliente.js
+++ view/NuevoCliente.js
@@ -1,13 +1,56 @@
-import { Text, View } from 'react-native';
-import { Headline } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native';
+import { Headline, TextInput } from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
+  const leerNombre = () => {
+    console.log('Leyendo nombre del cliente');
+  };
+
+  const leerTelefono = () => {
+    console.log('Leyendo teléfono del cliente');
+  };
+
   return (
     <View style={globalStyles.contenedor}>
       <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
+
+      <TextInput
+        label="Nombre"
+        placeholder="Nombre del cliente"
+        onChangeText={() => leerNombre()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Teléfono"
+        placeholder="Teléfono del cliente"
+        onChangeText={() => leerTelefono()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Correo"
+        placeholder="Correo del cliente"
+        onChangeText={() => leerCorreo()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Empresa"
+        placeholder="Empresa del cliente"
+        onChangeText={() => leerEmpresa()}
+        style={styles.input}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default NuevoCliente;
