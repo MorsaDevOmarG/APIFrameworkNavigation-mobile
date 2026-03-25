dda986e9290dd1fcdcb2932421110c7684ac9ba0
Omar <omar.garcia@morsa.com.mx>
2026-03-25 09:59:30 -0600

    CReando GLOBAL STYLE

---
+++ styles/global.js
@@ -0,0 +1,10 @@
+const globalStyles = {
+  titulo: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 30,
+    fontSize: 30,
+  },
+};
+
+export default globalStyles;
--- view/NuevoCliente.js
+++ view/NuevoCliente.js
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import { Headline } from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
   return (
     <View>
-      <Text>Nuevo Cliente</Text>
+      <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
     </View>
   );
 };
