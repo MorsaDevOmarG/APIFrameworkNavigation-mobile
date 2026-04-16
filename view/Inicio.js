b66343999cce6749073c43a2d51a79d3b06ff687
Omar <omar.garcia@morsa.com.mx>
2026-04-16 13:53:29 -0600

    Consultando la API para obtener CCLIENTES

--- view/Inicio.js
+++ view/Inicio.js
@@ -1,6 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
 const Inicio = () => {
+  const [clientes, guardarClientes] = useState([]);
+
+  useEffect(() => {
+    console.log('Componente Inicio montado');
+
+    const obtenerClientesApi = async () => {
+      try {
+        const resultado = await axios.get('http://10.0.2.2:3000/clientes');
+        console.log('Clientes obtenidos:', resultado.data);
+        guardarClientes(resultado.data);
+      } catch (error) {
+        console.log('ERROR RED:', error.message);
+      }
+    };
+
+    obtenerClientesApi();
+  }, []);
+
   return (
     <View>
       <Text>Desde Inicio</Text>
