707f5d988c8f59a6bda53c06ed8a16c6d934ac82
Omar <omar.garcia@morsa.com.mx>
2026-04-16 22:26:55 -0600

    Mostrando DETALLES de CLIENTE

--- view/DetallesCliente.js
+++ view/DetallesCliente.js
@@ -1,12 +1,36 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Headline, Subheading } from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const DetallesCliente = ({ route }) => {
   console.log('DetallesCliente - Route:', route.params);
+
+  const { nombre, telefono, correo, empresa } = route.params.item;
+
   return (
-    <View>
-      <Text>Detalles Cliente</Text>
+    <View style={globalStyles.contenedor}>
+      <Headline style={globalStyles.titulo}>{nombre}</Headline>
+
+      <Text style={styles.texto}>
+        Empresa: <Subheading>{empresa}</Subheading>
+      </Text>
+      <Text style={styles.texto}>
+        Teléfono: <Subheading>{telefono}</Subheading>
+      </Text>
+      <Text style={styles.texto}>
+        Correo: <Subheading>{correo}</Subheading>
+      </Text>
+
+      <Button>Eliminar Cliente</Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  texto: {
+    marginBottom: 20,
+    fontSize: 18,
+  },
+});
 
 export default DetallesCliente;
